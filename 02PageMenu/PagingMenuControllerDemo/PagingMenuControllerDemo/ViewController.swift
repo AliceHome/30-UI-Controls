7f4a70897248c3090fbd3dba3dc49e872027f4d8
Maple <huidragonaijy@gmail.com>
2019-02-26 21:09:43 +0800

    add pageMenu init

---
+++ 02PageMenu/PagingMenuControllerDemo/PagingMenuControllerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingMenuControllerDemo
+//
+//  Created by Maple on 2019/2/26.
+//  Copyright © 2019 Maple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
